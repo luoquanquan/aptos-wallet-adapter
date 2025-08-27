f76ea3a9592778a3ddbf4b02bc48a3ca00474baf
luoquanquan <luo_quanquan@163.com>
2025-08-28 00:53:14 +0800

    update

--- apps/nextjs-example/next.config.mjs
+++ apps/nextjs-example/next.config.mjs
@@ -1,12 +1,13 @@
 const isProd = process.env.NODE_ENV === "production";
+const isVercel = process.env.NODE_ENV_VERCEL === "1";
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
   reactStrictMode: true,
   transpilePackages: ["wallet-adapter-react", "wallet-adapter-plugin"],
-  assetPrefix: isProd ? "/aptos-wallet-adapter" : "",
-  basePath: isProd ? "/aptos-wallet-adapter" : "",
+  assetPrefix: isProd ? (isVercel ? "" : "/aptos-wallet-adapter") : "",
+  basePath: isProd ? (isVercel ? "" : "/aptos-wallet-adapter") : "",
   webpack: (config) => {
     config.resolve.fallback = { "@solana/web3.js": false };
     return config;
